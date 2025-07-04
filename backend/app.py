dc6850ba8686a2d8918d74ac5f8d2d32fc053c5d
Dominic Joaquin Dublado <dublado.dominicjoaquin@gmail.com>
2025-07-04 11:39:42 +0800

    Remove app.run() for Gunicorn deployment on Render

--- backend/app.py
+++ backend/app.py
@@ -135,7 +135,4 @@ def serve_react(path):
     if path != "" and os.path.exists(os.path.join('build', path)):
         return send_from_directory('build', path)
     else:
-        return send_from_directory('build', 'index.html')
-
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 9000))) 
+        return send_from_directory('build', 'index.html') 
